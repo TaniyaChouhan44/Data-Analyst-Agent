5a802968bce103e25614434fd89fdedf5027b5cb
Taniyachouhaniitm <taniyachouhan303@gmail.com>
2025-08-03 16:36:22 +0530

    Initial commit for TDS_PROJECT_2 files

---
+++ app.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+app=FastAPI()
+
+#app.add_middleware(CORSMiddleware, allow_origins=["*"])
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+    )
+
+
+@app.get('/')
+async def root():
+    return {"message": "Hello!"}
+
+@app.post('/api')
+async def upload_file(file :UploadFile=File(...)):
+    try:
+        content= await file.read()
+        text = content.decode("uft-8")
+        return {"filename": file.filename, "content":text}
+    except Exception as e:
+        return JSONResponse(status_code=400,content={"error":str(e)})
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
